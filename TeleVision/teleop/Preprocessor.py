01e42dd8f1c4ff2fb13ec4299f30e695afa32934
siqisiqisiqisiqi <szheng2@g.clemson.edu>
2025-07-04 09:53:55 -0400

    Added the comments to the repo

--- TeleVision/teleop/Preprocessor.py
+++ TeleVision/teleop/Preprocessor.py
@@ -19,48 +19,49 @@ class VuerPreprocessor:
                                              [0, 0, 1, -0.5],
                                              [0, 0, 0, 1]])
 
-    def process(self, tv):
-        self.vuer_head_mat = mat_update(
-            self.vuer_head_mat, tv.head_matrix.copy())
-        self.vuer_right_wrist_mat = mat_update(
-            self.vuer_right_wrist_mat, tv.right_hand.copy())
-        self.vuer_left_wrist_mat = mat_update(
-            self.vuer_left_wrist_mat, tv.left_hand.copy())
+    # def process(self, tv):
+    #     self.vuer_head_mat = mat_update(
+    #         self.vuer_head_mat, tv.head_matrix.copy())
+    #     self.vuer_right_wrist_mat = mat_update(
+    #         self.vuer_right_wrist_mat, tv.right_hand.copy())
+    #     self.vuer_left_wrist_mat = mat_update(
+    #         self.vuer_left_wrist_mat, tv.left_hand.copy())
 
-        # change of basis
-        head_mat = grd_yup2grd_zup @ self.vuer_head_mat @ fast_mat_inv(
-            grd_yup2grd_zup)
-        right_wrist_mat = grd_yup2grd_zup @ self.vuer_right_wrist_mat @ fast_mat_inv(
-            grd_yup2grd_zup)
-        left_wrist_mat = grd_yup2grd_zup @ self.vuer_left_wrist_mat @ fast_mat_inv(
-            grd_yup2grd_zup)
+    #     # change of basis
+    #     head_mat = grd_yup2grd_zup @ self.vuer_head_mat @ fast_mat_inv(
+    #         grd_yup2grd_zup)
+    #     right_wrist_mat = grd_yup2grd_zup @ self.vuer_right_wrist_mat @ fast_mat_inv(
+    #         grd_yup2grd_zup)
+    #     left_wrist_mat = grd_yup2grd_zup @ self.vuer_left_wrist_mat @ fast_mat_inv(
+    #         grd_yup2grd_zup)
 
-        rel_left_wrist_mat = left_wrist_mat @ hand2inspire
-        rel_left_wrist_mat[0:3, 3] = rel_left_wrist_mat[0:3,
-                                                        3] - head_mat[0:3, 3]
+    #     rel_left_wrist_mat = left_wrist_mat @ hand2inspire
+    #     rel_left_wrist_mat[0:3, 3] = rel_left_wrist_mat[0:3,
+    #                                                     3] - head_mat[0:3, 3]
 
-        rel_right_wrist_mat = right_wrist_mat @ hand2inspire  # wTr = wTh @ hTr
-        rel_right_wrist_mat[0:3,
-                            3] = rel_right_wrist_mat[0:3, 3] - head_mat[0:3, 3]
+    #     rel_right_wrist_mat = right_wrist_mat @ hand2inspire  # wTr = wTh @ hTr
+    #     rel_right_wrist_mat[0:3,
+    #                         3] = rel_right_wrist_mat[0:3, 3] - head_mat[0:3, 3]
 
-        # homogeneous
-        left_fingers = np.concatenate(
-            [tv.left_landmarks.copy().T, np.ones((1, tv.left_landmarks.shape[0]))])
-        right_fingers = np.concatenate(
-            [tv.right_landmarks.copy().T, np.ones((1, tv.right_landmarks.shape[0]))])
+    #     # homogeneous
+    #     left_fingers = np.concatenate(
+    #         [tv.left_landmarks.copy().T, np.ones((1, tv.left_landmarks.shape[0]))])
+    #     right_fingers = np.concatenate(
+    #         [tv.right_landmarks.copy().T, np.ones((1, tv.right_landmarks.shape[0]))])
 
-        # change of basis
-        left_fingers = grd_yup2grd_zup @ left_fingers
-        right_fingers = grd_yup2grd_zup @ right_fingers
+    #     # change of basis
+    #     left_fingers = grd_yup2grd_zup @ left_fingers
+    #     right_fingers = grd_yup2grd_zup @ right_fingers
 
-        rel_left_fingers = fast_mat_inv(left_wrist_mat) @ left_fingers
-        rel_right_fingers = fast_mat_inv(right_wrist_mat) @ right_fingers
-        rel_left_fingers = (hand2inspire.T @ rel_left_fingers)[0:3, :].T
-        rel_right_fingers = (hand2inspire.T @ rel_right_fingers)[0:3, :].T
+    #     rel_left_fingers = fast_mat_inv(left_wrist_mat) @ left_fingers
+    #     rel_right_fingers = fast_mat_inv(right_wrist_mat) @ right_fingers
+    #     rel_left_fingers = (hand2inspire.T @ rel_left_fingers)[0:3, :].T
+    #     rel_right_fingers = (hand2inspire.T @ rel_right_fingers)[0:3, :].T
 
-        return head_mat, rel_left_wrist_mat, rel_right_wrist_mat, rel_left_fingers, rel_right_fingers
+    #     return head_mat, rel_left_wrist_mat, rel_right_wrist_mat, rel_left_fingers, rel_right_fingers
 
     def process_v2(self, tv):
+        #  Transform Head and Wrist Poses from VR to Sim World
         self.vuer_head_mat = mat_update(
             self.vuer_head_mat, tv.head_matrix.copy())
         self.vuer_right_wrist_mat = mat_update(
@@ -68,7 +69,7 @@ class VuerPreprocessor:
         self.vuer_left_wrist_mat = mat_update(
             self.vuer_left_wrist_mat, tv.left_hand.copy())
 
-        # change of basis
+        # A change-of-basis matrix converting Y-up (common in VR) to Z-up (used in Sim environments like Isaac Sim).
         head_mat = grd_yup2grd_zup @ self.vuer_head_mat @ fast_mat_inv(
             grd_yup2grd_zup)
         right_wrist_mat = grd_yup2grd_zup @ self.vuer_right_wrist_mat @ fast_mat_inv(
@@ -76,23 +77,27 @@ class VuerPreprocessor:
         left_wrist_mat = grd_yup2grd_zup @ self.vuer_left_wrist_mat @ fast_mat_inv(
             grd_yup2grd_zup)
 
+        # Transform Wrist Poses into Sim-relative Hand Frames
         rel_left_wrist_mat = left_wrist_mat @ hand2Sim
         rel_right_wrist_mat = right_wrist_mat @ hand2Sim  # wTr = wTh @ hTr
 
-        # homogeneous
+        # Prepare Landmark (Finger Tip) Points as Homogeneous Coordinates
         left_fingers = np.concatenate(
             [tv.left_landmarks.copy().T, np.ones((1, tv.left_landmarks.shape[0]))])
         right_fingers = np.concatenate(
             [tv.right_landmarks.copy().T, np.ones((1, tv.right_landmarks.shape[0]))])
 
-        # change of basis
+        # Transform Landmarks from VR Y-up to Sim Z-up World
         left_fingers = grd_yup2grd_zup @ left_fingers
         right_fingers = grd_yup2grd_zup @ right_fingers
 
+        # Transform Landmarks into Wrist-relative Frame
         rel_left_fingers = fast_mat_inv(left_wrist_mat) @ left_fingers
         rel_right_fingers = fast_mat_inv(right_wrist_mat) @ right_fingers
+
+        #  Final Basis Change into "Inspire" Hand Convention
+        # (attention: hand2inspire should be renamed as inspire2hand)
         rel_left_fingers = (hand2inspire.T @ rel_left_fingers)[0:3, :].T
         rel_right_fingers = (hand2inspire.T @ rel_right_fingers)[0:3, :].T
 
         return head_mat, rel_left_wrist_mat, rel_right_wrist_mat, rel_left_fingers, rel_right_fingers
-
--- data_collection/run.sh
+++ data_collection/run.sh
@@ -8,11 +8,11 @@
 # Navigate to your workspace source folder
 cd ~/vr_ros2_ws/src/data_collection
 
-# Launch each script in the background
-echo "[INFO] Running initial_pose..."
-python3 ../pinocchio_ik_solver/pinocchio_ik_solver/initial_pose_publisher.py &
+# # Launch each script in the background
+# echo "[INFO] Running initial_pose..."
+# python3 ../pinocchio_ik_solver/pinocchio_ik_solver/initial_pose_publisher.py &
 
-sleep 5  # Optional: let initial_pose finish
+# sleep 5  # Optional: let initial_pose finish
 
 # echo "[INFO] Running GUI slider..."
 # python3 ../pinocchio_ik_solver/pinocchio_ik_solver/slider_gui_pose.py &
@@ -23,8 +23,8 @@ python3 data_collection/keyboard_pose_teleop.py &
 echo "[INFO] Running IK solver..."
 python3 ../pinocchio_ik_solver/pinocchio_ik_solver/clik_keyboard_ros.py &
 
-echo "[INFO] Running data collector..."
-python3 data_collection/data_collection.py &
+# echo "[INFO] Running data collector..."
+# python3 data_collection/data_collection.py &
 
 # Wait for all background jobs to finish
 wait
